a65258c490e2c562fdc916b4347e1df3dc952bfa
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2020-01-15 21:23:19 +0000

    initial commit

---
+++ src/main/java/com/andriusk/springbootpetclinic/SpringbootPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.andriusk.springbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andriusk/springbootpetclinic/SpringbootPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriusk.springbootpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
